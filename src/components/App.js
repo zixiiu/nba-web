67ea5000e3a03813fecb337dd99d6c152f13b16d
0d2_100004383376_zixiu <0d2_100004383376_zixiu@git.cloud.tencent.com>
2019-11-13 23:11:05 -0500

    folder REORDER

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../assets/images/nba-logoman-word-white.svg';
+import '../styles/App.css';
+import {TopNavBar} from "./TopNavBar"
+
+function App() {
+  return (
+      <div className="App">
+        <TopNavBar />
+      </div>
+  );
+}
+
+export default App;
